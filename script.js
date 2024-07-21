14e386069d8a442fd3721d7b33535c903eeefe6e
Sandrah <comfortsandrah13@gmail.com>
2024-07-21 23:54:24 +0300

    Refactor addTask function for readability and consistency

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const storedTasks = localStorage.getItem('tasks');
+    function loadTasks() {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+    }
     const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
-    function addTask() {
+    function addTask(taskText, save = true) {
         const taskText = taskInput.value.trim();
 
         if (taskText === "") {
@@ -19,18 +24,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
         btn.onclick = function () {
             taskList.removeChild(li);
+            const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+            const newTasks = storedTasks.filter((task) => task != taskText)
+            localStorage.setItem('tasks', JSON.stringify(newTasks));
         };
 
         li.appendChild(btn);
         taskList.appendChild(li);
         taskInput.value = "";
-        }
 
-    addButton.addEventListener("click", addTask);
+        if (save) {
+            const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+            storedTasks.push(taskText);
+            localStorage.setItem('tasks', JSON.stringify(storedTasks));
+        }
+    }
+
+    addButton.addEventListener("click", () => {
+        const taskText = taskInput.value.trim();
+        addTask(taskText)
+    });
     taskInput.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
-            addTask();
+            const taskText = taskInput.value.trim();
+            addTask(taskText);
         }
 
     });
+    loadTasks();
 });
